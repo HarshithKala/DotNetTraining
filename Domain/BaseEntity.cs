795380d35475251f90feb6642d6770dbd95f4ebd
HungryMinds <hungryminds217@gmail.com>
2024-01-15 00:37:23 +0000

    Changes related to domain project

---
+++ Domain/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain
+{
+    public class BaseEntity
+    {
+
+        public DateTime CreatedOn { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime UpdatedOn { get; set; }
+
+        public string? UpdatedBy { get; set; }
+
+    }
+}
---
+++ Domain/EFCrudOperationContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class EFCrudOperationContext : DbContext
+    {
+        public EFCrudOperationContext(DbContextOptions<EFCrudOperationContext> options) : base(options)
+        {
+            
+        }
+
+
+
+        public DbSet<Employee> Employees { get; set; }
+
+
+    }
+
+
+}
